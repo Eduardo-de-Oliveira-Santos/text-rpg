9fbda48f57b789600122d5a4ddf364ff7a276b0c
Eduardo-de-Oliveira-Santos <kratos010307@gmail.com>
2020-05-02 15:20:51 -0300

    commit

---
+++ Hospital.py
@@ -0,0 +1,39 @@
+import os
+import Status
+import vars
+import Quarto
+import Cidade
+
+CLEAR = 'cls' if os.name == 'nt' else 'clear'
+limpa_tela = lambda : os.system(CLEAR)
+
+def hospital():
+    while 1:
+        if vars.vida < vars.vida_max:
+            limpa_tela()
+            print("ola bem vindo ao hospital")
+            print("voce parece machucado voce quer se curar?")
+            print("1- sim")
+            print("2- nao")
+            vars.text = int(input("oque fazer? "))
+
+            if vars.text == 0:
+                limpa_tela()
+                Status.status()
+            elif vars.text == 1:
+                limpa_tela()
+                vars.vida = vida_max
+                print("voce esta curado ate mais")
+                return
+            if vars.text == 2:
+                limpa_tela()
+                print("ok adeus")
+                return
+        if vars.vida >= vars.vida_max:
+            limpa_tela()
+            print("voce ja esta saldavel nao a porque ficar aqui")
+            return
+        input('Enter para continuar')
+
+if __name__ == '__main__':
+     hospital()
