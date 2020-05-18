3317ba9b9a237aca8e14e4eb4b145815325c2e28
Eduardo-de-Oliveira-Santos <kratos010307@gmail.com>
2020-05-17 21:30:01 -0300

    Commit

--- Dividir_Pontos.py
+++ Dividir_Pontos.py
@@ -23,36 +23,44 @@ def D_Pontos():
             Player.agilidade += 1
             Player.pontos -= 1
             limpa_tela()
+             
         elif Variaveis.text == 2:
             Player.furtividade += 1
             Player.pontos -= 1
             limpa_tela()
+             
         elif Variaveis.text == 3:
             Player.sorte += 1
             Player.pontos -= 1
             limpa_tela()
+             
         elif Variaveis.text == 4:
             Player.labia += 1
             Player.pontos -= 1
             limpa_tela()
+             
         elif Variaveis.text == 5:
             Player.inteligencia += 1
             Player.pontos -= 1
             limpa_tela()
+             
         elif Variaveis.text == 6:
             Player.força += 1
             Player.pontos -= 1
             limpa_tela()
+             
         elif Variaveis.text == 7:
             Player.mira += 1
             Player.pontos -= 1
             limpa_tela()
+             
         elif Variaveis.text == 8:
             Player.carisma += 1
             Player.pontos -= 1
             limpa_tela()
         if Player.pontos == 0:
             Quarto.game()
+            
 
         input('Enter para continuar')
 
--- ferramentas_diverças.py
+++ ferramentas_diverças.py
@@ -1,3 +1,4 @@
 import os
+
 CLEAR = 'cls' if os.name == 'nt' else 'clear' 
 limpa_tela = lambda : os.system(CLEAR)
